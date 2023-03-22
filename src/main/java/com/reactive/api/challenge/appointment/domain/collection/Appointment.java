f2c86f6cd0985e35822551c3cbaadfc4c4329b2d
Juangmcz <juangui-089@hotmail.com>
2023-03-22 10:22:37 -0500

    Domain package was implemented.

---
+++ src/main/java/com/reactive/api/challenge/appointment/domain/collection/Appointment.java
@@ -0,0 +1,28 @@
+package com.reactive.api.challenge.appointment.domain.collection;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "appointments")
+public class Appointment {
+
+    private String id;
+    @NotNull(message = "The date cannot be null")
+    private String date;
+    @NotNull(message = "The barber cannot be null")
+    private String barberName;
+    private Boolean customerArrived = false;
+    private Boolean isFulFilled = false;
+
+    public Appointment(String date, String barberName) {
+        this.id = UUID.randomUUID().toString().substring(0, 10);
+        this.date = date;
+        this.barberName = barberName;
+    }
+}
---
+++ src/main/java/com/reactive/api/challenge/appointment/domain/dto/AppointmentDTO.java
@@ -0,0 +1,23 @@
+package com.reactive.api.challenge.appointment.domain.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class AppointmentDTO {
+
+    private String id;
+    @NotNull(message = "The date cannot be null")
+    private String date;
+    @NotNull(message = "The barber cannot be null")
+    private String barberName;
+    private Boolean customerArrived;
+    private Boolean isFulFilled;
+
+    public AppointmentDTO(String date, String barberName) {
+        this.date = date;
+        this.barberName = barberName;
+    }
+}
