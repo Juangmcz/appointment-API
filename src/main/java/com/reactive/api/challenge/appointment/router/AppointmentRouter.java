35a7d1d74ea2456aab07d5b7adbc8e9c7847411b
Juangmcz <juangui-089@hotmail.com>
2023-03-22 10:44:11 -0500

    AppointmentRouter was implemented.

---
+++ src/main/java/com/reactive/api/challenge/appointment/router/AppointmentRouter.java
@@ -0,0 +1,70 @@
+package com.reactive.api.challenge.appointment.router;
+
+import com.reactive.api.challenge.appointment.domain.dto.AppointmentDTO;
+import com.reactive.api.challenge.appointment.usecases.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class AppointmentRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> getAllAppointments(GetAllAppointmentsUseCase getAllAppointmentsUseCase){
+        return route(GET("api/appointments"),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getAllAppointmentsUseCase.get(), AppointmentDTO.class))
+                        .onErrorResume(throwable -> ServerResponse.noContent().build()));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> getAppointmentById(GetAppointmentById getAppointmentById){
+        return route(GET("api/appointments/{id}"),
+                request -> getAppointmentById.apply(request.pathVariable("id"))
+                        .flatMap(appointmentDTO -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(appointmentDTO))
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(throwable.getMessage())));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> saveAppointment(SaveAppointmentUseCase saveAppointmentUseCase){
+        return route(POST("api/appointments").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(AppointmentDTO.class)
+                        .flatMap(appointmentDTO -> saveAppointmentUseCase.apply(appointmentDTO)
+                                .flatMap(result -> ServerResponse.status(201)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).bodyValue(throwable.getMessage()))));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> updateAppointment(UpdateAppointmentUseCase updateAppointmentUseCase){
+        return route(PUT("api/appointments/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(AppointmentDTO.class)
+                        .flatMap(appointmentDTO -> updateAppointmentUseCase.apply(request.pathVariable("id"), appointmentDTO)
+                                .flatMap(result -> ServerResponse.status(200)
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .bodyValue(result))
+                                .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_ACCEPTABLE).bodyValue(throwable.getMessage()))));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> deleteAppointment(DeleteAppointmentUseCase deleteAppointmentUseCase){
+        return route(DELETE("api/appointments/{id}"),
+                request ->  deleteAppointmentUseCase.apply(request.pathVariable("id"))
+                        .flatMap(result -> ServerResponse.status(204)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(result))
+                        .onErrorResume(throwable -> ServerResponse.status(HttpStatus.NOT_FOUND).build()));
+    }
+}
